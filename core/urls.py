954d2cba5ed5da6a590cc8fd20ded0ea76a60b9d
pablo montenegro <pablo.rpm@gmail.com>
2020-10-23 13:55:58 -0300

    redirected

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from core.views import HomeView, JoinUsView, PageView, CountriesListView, PhotoKudoView, robots_txt, JoinUsThankYouView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
@@ -10,7 +11,7 @@ urlpatterns = [
     path('privacy-policy/', PageView.as_view(), {'page_name': 'our-privacy-policy'}, name='privacy-policy'),
     path('photo-guidelines/', PageView.as_view(), {'page_name': 'photo-guidelines'}, name='photo-guidelines'),
     path('careers/', PageView.as_view(), {'page_name': 'careers'}, name='careers'),
-    path('add-my-company/', PageView.as_view(), {'page_name': 'adding-your-business'}, name='add-my-company'),
+    path('add-my-company/', RedirectView.as_view(url='/join'), name='add-my-company'),
     path('tanzania-based-freelance-writer/', PageView.as_view(), {'page_name': 'tanzania-based-freelance-writer'},name='tanzania-based-freelance-writer'),
     path('advertise-with-us/', PageView.as_view(), {'page_name': 'advertise-with-us'}, name='advertise-with-us'),
     path('api/kudo/photo/<int:photo_pk>/', PhotoKudoView.as_view()),
